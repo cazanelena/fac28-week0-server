f62e5fc74c2cda3a93ea14358aedac8f0d86f363
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-09-15 15:07:47 +0100

    solved challenge 1

--- src/server.js
+++ src/server.js
@@ -2,4 +2,21 @@ const express = require("express");
 
 const server = express();
 
+server.get("/", (req, res) => {
+    res.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+      </head>
+      <body>
+        <h1>Hello Express</h1>
+      </body>
+    </html>
+  `);
+})
+
+
+
 module.exports = server;
